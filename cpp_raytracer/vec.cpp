ff14f43a4665c1dbe8558b8c81e9dac9a585d8aa
Lars Hebenstiel <lars.hebenstiel@gmail.com>
2020-11-29 11:56:12 -0500

    work on vec3d

--- cpp_raytracer/vec.cpp
+++ cpp_raytracer/vec.cpp
@@ -1,25 +1,51 @@
-#include "vec.h"
+#include <iostream>
 #include <cmath>
+#include "vec.h"
 
-vec3::vec3(double x, double y, double z) {
-    this->x = x;
-    this->y = y;
-    this->z = z;
+using std::cout;
+using std::cin;
+using std::endl;
+
+vec3d::vec3d() : v{ 0 } {}
+vec3d::vec3d(const double e) : v{ e } {}
+vec3d::vec3d(const double x, const double y, const double z) : v{ x, y, z } {}
+vec3d::vec3d(const vec3d& u) : v{ u.x, u.y, u.z } {}
+vec3d::~vec3d() {}
+
+std::ostream& operator<<(std::ostream& os, const vec3d& v) {
+	os << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+	return os;
 }
 
-vec3::vec3() {
-    this->x = 0;
-    this->y = 0;
-    this->z = 0;
-}
+double vec3d::operator[](const int i) const {return v[i];}
+double& vec3d::operator[](const int i) {return v[i];}
 
-double vec3::norm() {
-    return sqrt(pow(this->x, 2) + pow(this->y, 2) + pow(this->z, 2));
-}
+void vec3d::operator=(const vec3d& v) {this->x = v.x; this->y = v.y; this->z = v.z;}
+void vec3d::operator=(const double v[3]) {this->x = v[0]; this->y = v[1]; this->z = v[2];}
 
-void vec3::normalize() {
-    double norm = this->norm();
-    this->x /= norm;
-    this->y /= norm;
-    this->z /= norm;
+bool vec3d::operator==(const vec3d& v) const {return this->x == v.x && this->y == v.y && this->z == v.z;}
+bool vec3d::operator!=(const vec3d& v) const {return !(*this == v);}
+
+vec3d vec3d::operator+(const vec3d& v) const {return vec3d(this->x + v.x, this->y + v.y, this->z + v.z);}
+void vec3d::operator+=(const vec3d& v) {this->x += v.x; this->y += v.y; this->z += v.z;}
+
+vec3d vec3d::operator-(const vec3d& v) const {return vec3d(this->x - v.x, this->y - v.y, this->z - v.z);}
+void vec3d::operator-=(const vec3d& v) {this->x -= v.x; this->y -= v.y; this->z -= v.z;}
+
+//vector scalar product
+vec3d vec3d::operator*(const double s) const {return vec3d(s * this->x, s * this->y, s * this->z);}
+//vector scalar division
+vec3d vec3d::operator/(const double s) const {return vec3d(this->x / s, this->y / s, this->z / s);}
+
+//vector inner product
+double vec3d::operator*(const vec3d& v) const {return this->x * v.x + this->y * v.y + this->z + v.z;}
+//vector outer product
+vec3d vec3d::operator&(const vec3d& v) const {return vec3d(this->y*v.z - this->z*v.y, this->z*v.x - this->x*v.z, this->x*v.y - this->y*v.x);}
+//in place vector outer product
+void vec3d::operator&=(const vec3d& v) {*this = *this & v;}
+
+int main() {
+    vec3d u(1,2,3);
+
+    cout << u << endl;
 }
--- cpp_raytracer/vec.h
+++ cpp_raytracer/vec.h
@@ -1,15 +1,74 @@
 #ifndef VEC_H
 #define VEC_H
 
-class vec3 {
+class vec3d {
     public:
-        double x;
-        double y;
-        double z;
-        vec3();
-        vec3(double x, double y, double z);
-        double norm();
-        void normalize();
+        //data
+        union {
+            struct{
+                double x;
+                double y;
+                double z;
+            };
+            double v[3];
+        };
+
+        //size of vector
+        const static int n = 3;
+
+        //default constructor
+        vec3d();
+        //constructor: initializes to constant array with double e
+        vec3d(const double e);
+        //constructor: intializes to given values
+        vec3d(const double x, const double y, const double z);
+        //copy constructor
+        vec3d(const vec3d& v);
+        //destructor
+        ~vec3d();
+
+        // // printer method
+		// friend std::ostream& operator<<(std::ostream& os, const vec3d& v);
+
+        // //functions
+        // double norm() const;
+        // void normalize();
+
+        //read and write vec's with [] notation
+        double operator[](const int i) const;
+        double& operator[](const int i);
+
+        //copy assignment to vector
+        void operator=(const vec3d& v);
+        //copy assignment to array
+        void operator=(const double v[3]);
+
+        //vector equality
+        bool operator==(const vec3d& v) const;
+        //vector inequality
+        bool operator!=(const vec3d& v) const;
+
+        //vector addition
+        vec3d operator+(const vec3d& v) const;
+        //in place vector addition
+        void operator+=(const vec3d& v);
+
+        //vector subtraction
+        vec3d operator-(const vec3d& v) const;
+        //in place vector subtraction
+        void operator-=(const vec3d& v);
+
+        //vector scalar product
+        vec3d operator*(const double s) const;
+        //vector scalar division
+        vec3d operator/(const double s) const;
+
+        //vector inner product
+        double operator*(const vec3d& v) const;
+        //vector outer product
+        vec3d operator&(const vec3d& v) const;
+        //in place vector outer product
+        void operator&=(const vec3d& v);
 };
 
 #endif
