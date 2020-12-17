2135b4cd06be0d0102c233d0572ae8307fc1db02
Lars Hebenstiel <lars.hebenstiel@gmail.com>
2020-12-17 16:47:45 -0500

    Fixed random_double(), set up final scene. random_double() used keyword static on local var incorrectly. It would use the first (min,max) specified, instead of adapting.

--- src/main.cpp
+++ src/main.cpp
@@ -31,12 +31,58 @@ color ray_color(const ray& r, const hittable& world, int depth) {
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
+hittable_list random_scene() {
+    hittable_list world;
+
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3d(0,-1000,0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3d center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+            if ((center - point3d(4, 0.2, 0)).norm() > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3d(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3d(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3d(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
 int main() {
     // Image properties
-    const double aspect_ratio = 4.0 / 3.0;
-    const int image_width = 300;
+    const double aspect_ratio = 3.0 / 2.0;
+    const int image_width = 600;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int MSAA_samples_per_pixel = 1;
+    const int MSAA_samples_per_pixel = 4;
     const int MC_samples_per_pixel = 16;
     const int MSAA_subpixel_width = static_cast<int>(sqrt(MSAA_samples_per_pixel));
     const int max_depth = 20;
@@ -46,43 +92,14 @@ int main() {
     const double MSAA_subpixel_size = 1.0 / static_cast<double>(MSAA_subpixel_width);
     
     // World
-
-    hittable_list world;
-
-    auto material_ground = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    // auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    // auto material_left   = make_shared<dielectric>(1.5);
-    // auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
-
-    // world.add(make_shared<sphere>(point3d( 0.0, -100.5, -1.0), 100.0, material_ground));
-    // world.add(make_shared<sphere>(point3d( 0.0,    0.0, -1.0),   0.5, material_center));
-    // world.add(make_shared<sphere>(point3d(-1.0,    0.0, -1.0),   0.5, material_left));
-    // world.add(make_shared<sphere>(point3d(-1.0,    0.0, -1.0), -0.45, material_left));
-    // world.add(make_shared<sphere>(point3d( 1.0,    0.0, -1.0),   0.5, material_right));
-    world.add(make_shared<sphere>(point3d( 0.0, -1000.5, -1.0), 1000.0, material_ground));
-    // world.add(make_shared<sphere>(point3d( 0.0,    0.0, 1.0),   0.5, material_center));
-    // world.add(make_shared<sphere>(point3d(-0.5,    0.0, -1.0),   0.5, material_left));
-    // //world.add(make_shared<sphere>(point3d(-0.5,    0.0, -1.0),  -0.4, material_left));
-    // world.add(make_shared<sphere>(point3d( 1.0,    0.0, -1.0),   0.5, material_right));
-    
-    auto skymat = make_shared<lambertian>(color(0,0.4,0.5));
-    world.add(make_shared<sphere>(point3d(0.0, 12.0,10.0),10.0, skymat));
-
-    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3d(-4, 1, 0), 1.0, material2));
-
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3d(4, 1, 0), 1.0, material3));
-
-    auto material1 = make_shared<dielectric>(10);
-    world.add(make_shared<sphere>(point3d(0, 1, 0), 1.0, material1));
+    hittable_list world = random_scene();
 
     // Camera
     point3d lookfrom(13,2,3);
-    point3d lookat(0,2,0);
+    point3d lookat(0,1,0);
     vec3d vup(0,1,0);
     auto dist_to_focus = 10.0;
-    auto aperture = 0.0;
+    auto aperture = 0.1;
 
     camera cam(lookfrom, lookat, vup, 20.0, aspect_ratio, aperture, dist_to_focus);
 
